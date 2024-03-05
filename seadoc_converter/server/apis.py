132febfd470ab010ae04a0fcf94a2f010fe46417
欢乐马 <38058090+SkywalkerSpace@users.noreply.github.com>
2024-03-05 11:37:06 +0800

    fix convert_markdown_to_sdoc

--- seadoc_converter/server/apis.py
+++ seadoc_converter/server/apis.py
@@ -74,7 +74,7 @@ def convert_markdown_to_sdoc():
         if file_content:
             file_content = md2sdoc(file_content, username=username)
         file_name = file_name[:-2] + 'sdoc'
-    if extension == '.docx' and src_type == 'docx' and dst_type == 'sdoc':
+    elif extension == '.docx' and src_type == 'docx' and dst_type == 'sdoc':
         if file_content:
             file_content = docx2sdoc(file_content, data)
         file_name = file_name[:-4] + 'sdoc'
