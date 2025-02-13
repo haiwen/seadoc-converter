e95d76e471de00d34f74dbddcc90f5588090a8bf
skywalker <dzmbbs@qq.com>
2025-02-13 12:05:26 +0800

    update seadoc env

--- seadoc_converter/config.py
+++ seadoc_converter/config.py
@@ -13,7 +13,8 @@ SERVER_PORT = '8888'
 SEADOC_PRIVATE_KEY = ''
 SEAHUB_SERVICE_URL = ''
 
-LOG_FILE = None
+LOG_DIR = os.environ.get('LOG_DIR', '')
+LOG_FILE = os.path.join(LOG_DIR, 'seadoc-converter.log')
 LOG_LEVEL = 'info'
 ENABLE_SYS_LOG = False
 
@@ -27,7 +28,7 @@ if not os.path.exists(SDOC_DIR):
     raise RuntimeError('SDOC_DIR does not exist.')
 sys.path.insert(0, SDOC_DIR)
 
-SDOC_OPERATION_CLEAN_LOG_FILE = 'sdoc_operation_log_clean.log'
+SDOC_OPERATION_CLEAN_LOG_FILE = os.path.join(LOG_DIR, 'sdoc_operation_log_clean.log')
 SDOC_OPERATION_CLEAN_LOG_LEVEL = 'info'
 
 
