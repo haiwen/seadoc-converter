3d54ba32d26836c442e0e2d703d9bf7d4919148d
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-06-27 13:39:48 +0800

    update

--- seadoc_converter/server/apis.py
+++ seadoc_converter/server/apis.py
@@ -340,4 +340,6 @@ def confluence_to_wiki():
     finally:
         if not is_same_machine:
             shutil.rmtree(extract_dir)
+        if os.path.exists(zip_file_path):
+            os.remove(zip_file_path)
     return {'cf_id_to_cf_title_map': cf_id_to_cf_title_map}, 200
